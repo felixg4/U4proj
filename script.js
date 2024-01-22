bb31112fe16fcbec6f6cba8d83918f57b0672961
felix <felixg10@nycstudents.net>
2024-01-22 11:01:32 -0500

    finished game logic i think

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const grid = document.getElementById('grid-container')
 const modal = document.getElementById('myModal')
-window.onclick = (e) => {if (e.target == modal) modal.style.display = 'none'}
+window.onclick = e => {if (e.target == modal) modal.style.display = 'none'}
 let numPieces = 15
 let gridLen = Math.floor(Math.sqrt(numPieces+1))
 // [piece that can move up, piece that can move down, piece that can move left, piece that can move right]
 const piecesThatCanMove = [null, null, null, null]
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i <= numPieces; i++) {
     const newBlock = document.createElement('span')
     newBlock.id = i
-    if (i < 15) {
+    if (i < numPieces) {
         newBlock.textContent = i + 1
         newBlock.className = 'block'
     }
@@ -25,33 +25,18 @@ for (const node of grid.childNodes) pieceArray.push(node.id)
     12, 13, 14,  15, where 15 is the blank
 ]
 */
-let emptyPosition = gridLen**2-1
+let emptyPosition = numPieces
 function updatePiecesThatCanMove() {
-    console.log(pieceArray)
-    for (let i = 0; i < piecesThatCanMove.length; i++) piecesThatCanMove[i] = null
     // only pieces bordering the 0 can move
-    emptyPosition = pieceArray.indexOf('15')
-    console.log(emptyPosition)
+    emptyPosition = pieceArray.indexOf(`${numPieces}`)
     // if the 15 is not at the right edge, the piece to the right of it can move left 
-    if ((emptyPosition + 1) % gridLen != 0) {
-        console.log(pieceArray[emptyPosition+1])
-        piecesThatCanMove[2] = pieceArray[emptyPosition+1]
-    }
+    piecesThatCanMove[2] = ((emptyPosition + 1) % gridLen != 0) ? pieceArray[emptyPosition+1] : null
     // if the 15 is not at the left edge, the piece to the left of it can move right
-    if ((emptyPosition - 1) % gridLen != 0) {
-        console.log(pieceArray[emptyPosition-1])
-        piecesThatCanMove[3] = pieceArray[emptyPosition-1]
-    }
+    piecesThatCanMove[3] = ((emptyPosition - 1) % gridLen != 0) ? pieceArray[emptyPosition-1] : null
     // if the 15 is not at the top edge, the piece above it can move down
-    if (emptyPosition >= gridLen) {
-        console.log(pieceArray[emptyPosition - gridLen])
-        piecesThatCanMove[1] = pieceArray[emptyPosition - gridLen]
-    }
+    piecesThatCanMove[1] = (emptyPosition >= gridLen) ? pieceArray[emptyPosition - gridLen] : null
     // if the 15 is not at the bottom edge, the piece below it can move up
-    if (emptyPosition < (gridLen*(gridLen - 1))) {
-        console.log(pieceArray[emptyPosition + gridLen])
-        piecesThatCanMove[0] = pieceArray[emptyPosition + gridLen]
-    }
+    piecesThatCanMove[0] = (emptyPosition < (gridLen*(gridLen - 1))) ? pieceArray[emptyPosition + gridLen] : null
     console.log(piecesThatCanMove)
 }
 function movePiece(id) {
@@ -59,7 +44,6 @@ function movePiece(id) {
     let direction = piecesThatCanMove.indexOf(id)
     if (direction < 0) return
     let pieceClickedIdx = pieceArray.indexOf(id)
-    
     let pieceClicked, placeHolderPiece
     switch (direction) {
         // swap 15 and the one below
@@ -75,8 +59,8 @@ function movePiece(id) {
         case 1:
             placeHolderPiece = pieceArray.splice(emptyPosition, 1)[0]
             pieceClicked = pieceArray.splice(pieceClickedIdx, 1)[0]
-            pieceArray.splice(emptyPosition, 0, pieceClicked)
             pieceArray.splice(pieceClickedIdx, 0, placeHolderPiece)
+            pieceArray.splice(emptyPosition, 0, pieceClicked)
             emptyPosition = pieceClickedIdx
             break
         // swap 15 and the one to its right
@@ -91,18 +75,20 @@ function movePiece(id) {
         case 3:
             placeHolderPiece = pieceArray.splice(emptyPosition, 1)[0]
             pieceClicked = pieceArray.splice(pieceClickedIdx, 1)[0]
-            pieceArray.splice(emptyPosition, 0, pieceClicked)
             pieceArray.splice(pieceClickedIdx, 0, placeHolderPiece)
+            pieceArray.splice(emptyPosition, 0, pieceClicked)
             emptyPosition = pieceClickedIdx
             break
     }
     for (const id of pieceArray) grid.appendChild(document.getElementById(id))
 }
 function scramble() {
-    let lastPieceMoved = Math.floor(Math.random() * 15)
+    let piecesThatCanActuallyMove = piecesThatCanMove.filter(x => x != null)
+    let lastPieceMoved = piecesThatCanActuallyMove[Math.floor(Math.random() * piecesThatCanActuallyMove.length)]
     for (let i = 0; i < 1000; i++) {
         movePiece(lastPieceMoved)
-        let newPiece =  Math.floor(Math.random() * 15)
+        piecesThatCanActuallyMove = piecesThatCanMove.filter(x => x != null)
+        let newPiece = piecesThatCanActuallyMove[Math.floor(Math.random() * piecesThatCanActuallyMove.length)]
         while (newPiece == lastPieceMoved) newPiece = Math.floor(Math.random() * 15)
         lastPieceMoved = newPiece
         movePiece(lastPieceMoved)
